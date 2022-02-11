161e5a5adb0f2b32f39aa9a68a3c2d1667ffd4d6
sarahwimark <sarahwimark@gmail.com>
2022-02-11 15:56:16 +0100

    Building restaurant info card

---
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Searchbar } from 'react-native-paper'
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native'
+
+export const RestaurantsScreen = () => {
+  return (
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.search}>
+        <Searchbar />
+      </View>
+      <View style={styles.list}>
+        <RestaurantInfo />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: 'purple',
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue',
+  },
+})
