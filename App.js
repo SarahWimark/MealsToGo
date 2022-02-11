90296501f2c32e4e6f2fbd85290c8d86b3220833
sarahwimark <sarahwimark@gmail.com>
2022-02-11 15:56:46 +0100

    .

--- App.js
+++ App.js
@@ -1,36 +1,12 @@
 import React from 'react'
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurant.screen'
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
-import { Searchbar } from 'react-native-paper'
-import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native'
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.safeAreaView}>
-        <View style={styles.search}>
-          <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <Text>ListItems...</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   )
 }
-
-const styles = StyleSheet.create({
-  safeAreaView: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: 'purple',
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-  },
-})
