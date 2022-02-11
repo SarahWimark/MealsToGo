9e1968aae8fe1c2eb20a5363a6cac752d9be11ad
sarahwimark <sarahwimark@gmail.com>
2022-02-11 16:21:41 +0100

    Renaming restaurant info component

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Text, StyleSheet } from 'react-native'
 import { Card } from 'react-native-paper'
 
-export const RestaurantInfo = ({ restaurant = {} }) => {
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const { name, icon, photos, address, isOpen, rating, isClosed } = restaurant
   return (
     <Card elevation={5} style={styles.card}>
       <Card.Cover
         key={name}
         style={StyleSheet.cover}
-        source={{ uri: photos[0] }}
+        // source={{ uri: photos[0] }}
       />
       <Text>{name}</Text>
     </Card>
