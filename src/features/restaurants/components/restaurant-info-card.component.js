0dbafadeff2545ead63a81427467ecbbd2f28ec4
sarahwimark <sarahwimark@gmail.com>
2022-02-11 16:35:35 +0100

    Adding styled components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Text, StyleSheet } from 'react-native'
 import { Card } from 'react-native-paper'
+
+const Title = styled.Text`
+  padding: 16px;
+`
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const { name, icon, photos, address, isOpen, rating, isClosed } = restaurant
@@ -11,7 +16,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         style={StyleSheet.cover}
         // source={{ uri: photos[0] }}
       />
-      <Text>{name}</Text>
+      <Title>{name}Some text</Title>
     </Card>
   )
 }
