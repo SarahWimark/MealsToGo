af619455953909f09a21fd48870d0d582d2fb2de
sarahwimark <sarahwimark@gmail.com>
2022-02-15 09:37:43 +0100

    Adding open icon

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -4,6 +4,7 @@ import { Text, StyleSheet } from 'react-native'
 import { SvgXml } from 'react-native-svg'
 import { Card } from 'react-native-paper'
 import star from '../../../../assets/star'
+import open from '../../../../assets/open'
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary}; ;
@@ -15,6 +16,8 @@ const RestaurantCardCover = styled(Card.Cover)`
 `
 const Rating = styled.View`
   flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `
 const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -28,15 +31,27 @@ const Address = styled.Text`
   font-family: ${(props) => props.theme.fonts.body};
   font-size: ${(props) => props.theme.fontSizes.caption};
 `
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name,
-    icon,
-    photos,
-    address,
-    isOpen,
+    name = 'Some Restaurant',
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
+    photos = [
+      'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
+    ],
+    address = '100 some random street',
+    isOpen = true,
     rating = 4,
-    isClosed,
+    isClosedTemporarily = true,
   } = restaurant
   const ratingArray = Array.from(new Array(Math.floor(rating)))
   return (
@@ -44,16 +59,21 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <RestaurantCardCover
         key={name}
         style={StyleSheet.cover}
-        // source={{ uri: photos[0] }}
+        source={{ uri: photos[0] }}
       />
       <Info>
-        <Title>{name}Some text</Title>
-        <Rating>
-          {ratingArray.map(() => (
-            <SvgXml xml={star} width={20} height={20} />
-          ))}
-        </Rating>
-        <Address>{address}Some text</Address>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isOpen && <SvgXml xml={open} width={20} height={20} />}
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
       </Info>
     </RestaurantCard>
   )
