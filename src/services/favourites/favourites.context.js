db357188ed43da72d1aea63c2fe20cef62752b0d
sarahwimark <sarahwimark@gmail.com>
2022-02-19 17:05:37 +0100

    Adding functionality to save and load favourites

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,9 +1,30 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favourites", jsonValue);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@storage_Key");
+      if (value !== null) {
+        setFavourites(JSON.parse(value));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const addFavourite = (restaurant) => {
     setFavourites([...favourites, restaurant]);
@@ -16,6 +37,14 @@ export const FavouritesContextProvider = ({ children }) => {
 
     setFavourites(newFavourites);
   };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   return (
     <FavouritesContext.Provider
