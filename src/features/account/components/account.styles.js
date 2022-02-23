8338eae82e98bc7361fa2353e50bde3da06ccb8c
sarahwimark <sarahwimark@gmail.com>
2022-02-23 16:51:58 +0100

    Adding animation for account sccreen

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -45,3 +45,11 @@ export const ErrorContainer = styled.View`
 export const Title = styled(Text)`
   font-size: 30px;
 `;
+
+export const AnimationWrapper = styled.View`
+  width: 100%;
+  height: 30%;
+  position: absolute;
+  top: 10px;
+  padding: ${(props) => props.theme.space[2]};
+`;
